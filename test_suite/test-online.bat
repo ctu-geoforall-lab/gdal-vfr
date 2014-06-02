d1772969aecb1272427a0a0bb01409f6375c3fa8
Martin Landa <martin.landa@fsv.cvut.cz>
2014-06-02 15:30:39 +0200

    update test_suite for Windows ogr

--- test_suite/test-online.bat
+++ test_suite/test-online.bat
@@ -2,10 +2,15 @@
 
 set DB=vfr
 set USER=postgres
-
-set PGM=pg
 set CONN=--username %USER%
-set OPT=--dbname %DB% --user %USER%
+
+IF "%1"=="ogr" (
+   set PGM=%1
+   set OPT=--format PostgreSQL --dsn="PG:dbname=%DB% user=%USER%"
+) ELSE (
+   set PGM=pg
+   set OPT=--dbname %DB% --user %USER%
+)
 
 set PATH=C:\Program Files (x86)\PostgreSQL\9.3\bin;%PATH%
 psql -d %DB% -f cleandb.sql %CONN% 2>nul
@@ -24,8 +29,12 @@ REM third pass (overwrite)
 echo "Third pass (overwrite...)"
 call vfr2%PGM% --type OB_564729_UKSH %OPT% --o
 
+REM fourth pass (append)
 echo "Forth pass (append...)"
 call vfr2%PGM% --type OB_554979_UKSH %OPT% --a
 
-echo "Fifth pass (schema per file...)"
-call vfr2%PGM% --type OB_564729_UKSH %OPT% -s
+if %PGM%==pg (
+   REM fifth pass (schema per file)
+   echo "Fifth pass (schema per file...)"
+   call vfr2%PGM% --type OB_564729_UKSH %OPT% -s
+)
