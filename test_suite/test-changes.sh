c24c39a14f5c413a069c2b4860e7d3337f9eef95
Martin Landa <landa.martin@gmail.com>
2014-07-30 21:19:39 +0200

    first version of changes test

---
+++ test_suite/test-changes.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DB=vfr
+
+# single file 
+if test -z "$1" ; then
+    PGM=pg
+    OPT="--dbname $DB"
+
+    psql -d $DB -f cleandb.sql 2>/dev/null
+else
+    if [ "$1" = "ogr" ] ; then
+        PGM=ogr
+        OPT="--format SQLite --dsn ${DB}.db"
+
+        rm -f ${DB}.db
+    else
+        PGM=oci
+        OPT="--user test --passwd test"
+    fi
+fi
+
+echo "Using vfr2${PGM}..."
+
+echo "1st PASS (empty DB...)"
+../vfr2${PGM}.py --file ST_ZKSH.xml.gz $OPT
+
+echo "2nd PASS (apply changes...)"
+../vfr2${PGM}.py --file ST_ZKSH.xml.gz $OPT
+
+exit 0
