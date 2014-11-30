8d57e79b75d5b20445be27b3de7401b6723b3c65
Martin Landa <landa.martin@gmail.com>
2014-11-30 17:34:42 +0100

    fix load_db.bat (download files into fulldb)

--- test_suite/load_db.bat
+++ test_suite/load_db.bat
@@ -1,9 +1,12 @@
 @echo off
 
 set DB=ruian
-set FILE=bin\test_suite\db_uksh.txt
+set DIR=%osgeo4w_root%\fulldb
+set FILE=%osgeo4w_root%\bin\test_suite\db_uksh.txt
 set USER=postgres
 set LOG=log.txt
 set LOG_ERR=log_err.txt
 
+if not exist "%DIR%" mkdir %DIR%
+cd %DIR%
 vfr2pg --file %FILE% --dbname %DB% --user %USER% --o >%LOG% 2>%LOG_ERR%
