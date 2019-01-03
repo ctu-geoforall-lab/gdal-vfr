ctu-geoforall-lab/gdal-vfr

commit 396f13ce6c8c43c787f632396bbf725f785ae0e8

cuzk changed compression method from gz to zip

vfr4ogr/vfrogr.py
@@ -31,7 +31,7 @@ except ImportError as e:
 
 from .exception import VfrError
 from .logger import VfrLogger
-from .utils import last_day_of_month, yesterday
+from .utils import last_day_of_month, yesterday, parse_xml, extension
 
 class Mode:
     """File open mode.
@@ -304,7 +304,9 @@ class VfrOgr:
         for line in file_list:
             if os.path.exists(line):
                 ftype, fencoding =  mimetypes.guess_type(line)
-                if ftype in ('application/xml', 'text/xml') and fencoding == 'gzip': # downloaded VFR file, skip
+                if ((ftype in ('application/xml', 'text/xml') and fencoding == 'gzip') or \
+                    (ftype == 'application/zip' and fencoding is None)):
+                    # downloaded VFR file, skip
                     self._file_list.append(os.path.abspath(line))
                 else:
                     VfrLogger.warning("File <{}>: unsupported minetype '{}'".format(line, ftype))
@@ -319,7 +321,10 @@ class VfrOgr:
                             date = last_day_of_month()
                     else:
                         date = force_date
-                    line = date + '_' + line
+                else:
+                    date = datetime.datetime.date(
+                        datetime.datetime.strptime(line.split('_', 1)[0], "%Y%m%d")
+                    )
 
                 if not line.startswith('http'):
                     # add base url if missing
@@ -331,9 +336,11 @@ class VfrOgr:
 
                     line = base_url_line + line
 
-                if not line.endswith('.xml.gz'):
+
+                ext = '.xml.{}'.format(extension())
+                if not line.endswith(ext):
                     # add extension if missing
-                    line += '.xml.gz'
+                    line += ext
 
                 self._file_list.append(self._download_vfr(line))
                
@@ -368,7 +375,8 @@ class VfrOgr:
 
         @return datasource instance
         """
-        self._ids = self._idrv.Open(filename, False)
+        vsi = '/vsizip/' if extension() == 'zip' else '/vsigzip/'
+        self._ids = self._idrv.Open(vsi + filename, False)
         if self._ids is None:
             raise VfrError("Unable to open file '%s'. Skipping.\n" % filename)
 
@@ -921,7 +929,7 @@ class VfrOgr:
                 # no output datasource given -> list available layers and exit
                 layer_list = self._list_layers(extended, sys.stdout)
                 if extended and os.path.exists(filename):
-                    compare_list(layer_list, parse_xml_gz(filename))
+                    compare_list(layer_list, parse_xml(filename))
             else:
                 if self.odsn is None:
                     self.odsn = '.' # current directory
@@ -943,7 +951,8 @@ class VfrOgr:
                     if self._schema_per_file or self._schema:
                         if self._schema_per_file:
                             # set schema per file
-                            schema_name = os.path.basename(fname).rstrip('.xml.gz').lower()
+                            ext = fname.split('.', 1)[1]
+                            schema_name = os.path.basename(fname).rstrip('.' + ext).lower()
                             if schema_name[0].isdigit():
                                 schema_name = 'vfr_' + schema_name
                         else:

vfr2ogr.py
@@ -41,7 +41,7 @@ def parse_args():
                         action='store_true',
                         help="List existing layers in output datasource and exit")
     parser.add_argument("--file",
-                        help="Path to xml.gz or URL list file")
+                        help="Path to xml.gz|zip or URL list file")
     parser.add_argument("--date",
                         help="Date in format 'YYYYMMDD'")
     parser.add_argument("--type",

vfr2pg.py
@@ -46,7 +46,7 @@ def parse_args():
                         action='store_true',
                         help="List existing layers in output database and exit")
     parser.add_argument("--file",
-                        help="Path to xml.gz or URL list file")
+                        help="Path to xml.gz|zip or URL list file")
     parser.add_argument("--date",
                         help="Date in format 'YYYYMMDD'")
     parser.add_argument("--type",

vfr4ogr/parse.py
@@ -11,7 +11,7 @@
 import os
 import sys
 
-from .utils import read_file, last_day_of_month, yesterday, get_date_interval, list_formats
+from .utils import read_file, last_day_of_month, yesterday, get_date_interval, list_formats, extension
 from .exception import VfrErrorCmd
 
 def parse_cmd(optdir):
@@ -76,7 +76,7 @@ def parse_cmd(optdir):
     else:                      # --date && --type
         file_list = []
         for d in date_list:
-            file_list.append("%s_%s.xml.gz" % (d, ftype))
+            file_list.append("{}_{}.xml.{}".format(d, ftype, extension()))
     
     if not file_list:
         raise VfrErrorCmd("Empty date range")

vfr4ogr/utils.py
@@ -10,7 +10,6 @@
 
 import os
 import sys
-import gzip
 import datetime
 import mimetypes
 from xml.dom.minidom import parse, parseString
@@ -75,7 +74,7 @@ def read_file(filename, date=None):
     
     return file_list
 
-def parse_xml_gz(filename):
+def parse_xml(filename):
     """Parse VFR (XML) file.
 
     @param filename: name of VFR file to be parsed
@@ -83,9 +82,17 @@ def parse_xml_gz(filename):
     @return list of items
     """
     VfrLogger.msg("Comparing OGR layers and input XML file (may take some time)...", header=True)
-    infile = gzip.open(filename)
-    content = infile.read()
-    
+    if date > datetime.date(2018, 12, 7):
+        from zipfile import ZipFile
+        with ZipFile(filename) as zipfile:
+            item = os.path.splitext(os.path.basename(filename))[0]
+            with zipfile.open(item) as fd:
+                content = fd.read()
+    else:
+        import gzip
+        with gzip.open(filename) as fd:
+            content = fd.read()
+
     # parse xml file content
     dom = parseString(content)
     data = dom.getElementsByTagName('vf:Data')[0]
@@ -167,4 +174,8 @@ def get_date_interval(date):
         d += delta
     
     return dlist
+
+def extension():
+    """Return valid file extension"""
+    return 'zip' if datetime.date.today() > datetime.date(2018, 12, 7) else 'gz'
 

commit 39871692355f669069dca3ff3c28dda3bb47cdfb

fix bug introduced in 396f13ce6c8c43

vfr4ogr/vfrogr.py
@@ -321,6 +321,7 @@ class VfrOgr:
                             date = last_day_of_month()
                     else:
                         date = force_date
+                    line = date + '_' + line
                 else:
                     date = datetime.datetime.date(
                         datetime.datetime.strptime(line.split('_', 1)[0], "%Y%m%d")